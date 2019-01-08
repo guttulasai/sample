226bb8e8152a271e73a44fb5d3189654cbce28f7
Sai Srinivas  Guttula <sai@Sais-MacBook-Pro.local>
2019-01-07 19:38:57 -0500

    sample

---
+++ app/src/main/java/com/example/sai/sampleapp/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.sai.sampleapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnClickMe;
+    EditText etEnter;
+    TextView tvView;
+
+    String st;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnClickMe=findViewById(R.id.btn);
+
+        etEnter=findViewById(R.id.et);
+
+        tvView=findViewById(R.id.tv);
+
+        btnClickMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                st=etEnter.getText().toString();
+
+                tvView.setText(st);
+            }
+        });
+    }
+}
